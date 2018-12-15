e67365477506f4980ebb845d89fb76874c014fb5
unionofficial2009 <38653752+unionofficial2009@users.noreply.github.com>
2018-12-15 22:33:37 +0800

    Update index.js

--- index.js
+++ index.js
@@ -53,21 +53,22 @@ let args =  messageArray.slice(1);
    ignchannel.fetchMessages({ limit: mmcount })
    .then(messages => {
       
+      message.reply(`${messages.size}`);
       //message.reply(messages.map(m=> `${m.createdAt.getMonth()+1}-${m.createdAt.getDate()}-${m.createdAt.getFullYear()}`).join(", ")) 
       //message.reply(messages.map(m=> `${m.embeds[0].createdAt}`).join(", "));
       //message.reply(messages.map(m=> `${m.embeds[0].fields[0].value}`).join(", "));
      
-      messages.forEach(function(message,messageid) {
+      //messages.forEach(function(message,messageid) {
         
-        if(message.embeds[0].fields[0].value == message.member.displayName){
-          myattendance = myattendance + 1;
-        }  
+     //   if(message.embeds[0].fields[0].value == message.member.displayName){
+     //     myattendance = myattendance + 1;
+     //   }  
         
-     })   
+    // })   
      
-     if (myattendance > 0){
-       return message.reply("You have to wait 1 day.")  
-     }  
+    // if (myattendance > 0){
+    //   return message.reply("You have to wait 1 day.")  
+    // }  
        
       message.delete().catch(O_o=>{});  
    }).catch(console.error);
