2aef5ae4d55800e15d4bd841f1218f1a4a6d03bd
unionofficial2009 <38653752+unionofficial2009@users.noreply.github.com>
2018-12-15 12:29:57 +0800

    Update index.js

--- index.js
+++ index.js
@@ -342,7 +342,13 @@ let args =  messageArray.slice(1);
        
       mlist = mlist + "\n\n📢 **Please select party forming roles here** :arrow_right: <#523341123289088000>";
       mlist = mlist + "\n📢 **Please remove party forming roles after you finished the party runs.**";
-  
+      mlist = mlist + "\n\n**Party Forming Commands:"; 
+      mlist = mlist + "\n1. &et1to50 - for Endless Tower floor 1-50";  
+      mlist = mlist + "\n2. &et51to90 - for Endless Tower floor 51-90"; 
+      mlist = mlist + "\n3. &dojo - for Guild Dojo Challenge"; 
+      mlist = mlist + "\n4. &vr40 - for Vahalla Ruins 40";  
+      mlist = mlist + "\n4. &vr60 - for Vahalla Ruins 60";
+       
       mlist = mlist + "\n\n<@&489776631913906199>";
       
       message.reply(`${mlist}`);
