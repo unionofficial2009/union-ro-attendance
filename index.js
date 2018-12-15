b45be16496a7009c437e5c29cb8842460a702436
unionofficial2009 <38653752+unionofficial2009@users.noreply.github.com>
2018-12-15 21:22:09 +0800

    Update index.js

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ let args =  messageArray.slice(1);
    .then(messages => {
       
        
-      message.reply(messages.map(m=> `${m.embeds[0].description}`).join(", "));
+      message.reply(messages.map(m=> `${m.embeds[0].fields[0].value}`).join(", "));
       message.delete().catch(O_o=>{});  
    }).catch(console.error);
    
